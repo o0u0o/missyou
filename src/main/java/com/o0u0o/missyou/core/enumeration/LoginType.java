ba544a7944703f42a7c4f7f489348c921408525f
岳川贵 <jobs@techgeng.com>
2021-02-09 16:03:24 +0800

    :apple: 登录枚举

---
+++ src/main/java/com/o0u0o/missyou/core/enumeration/LoginType.java
@@ -0,0 +1,16 @@
+package com.o0u0o.missyou.core.enumeration;
+
+/**
+ * @ClassName TokenController
+ * @Author o0u0o
+ * @UpdateUser o0u0o
+ * @Date 2021/2/9 3:58 下午
+ * @Descripton: 登录枚举
+ * @Version: v0.0.1
+ **/
+public enum LoginType {
+    USER_WX,
+    USER_EMAIL;
+
+
+}
