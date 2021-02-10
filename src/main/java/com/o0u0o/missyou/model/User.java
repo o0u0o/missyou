e1506681c4c2238833c2afaae4831de4e00645d9
岳川贵 <jobs@techgeng.com>
2021-02-10 11:54:38 +0800

    :apple: 新增用户实体

---
+++ src/main/java/com/o0u0o/missyou/model/User.java
@@ -0,0 +1,45 @@
+package com.o0u0o.missyou.model;
+
+import java.util.Map;
+
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@Builder
+@Where(clause = "delete_time is null")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** 微信openid */
+    private String openid;
+
+    /** 昵称 */
+    private String nickname;
+
+    private Integer unifyUid;
+
+    /** 电子邮箱 */
+    private String email;
+
+    /** 密码 建议把权限放在另外一张表 */
+    private String password;
+
+    /** 电话号 */
+    private String mobile;
+
+    /** 用户微信资料 */
+    @Convert(converter = MapAndJson.class)
+    private Map<String, Object> wxProfile;
+
+}
+
