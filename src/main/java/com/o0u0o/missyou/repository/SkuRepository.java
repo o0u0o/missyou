91ecf537d537cede8659115911cabf3c47813ae2
岳川贵 <jobs@techgeng.com>
2021-03-18 15:47:13 +0800

    :sparkle: 核销优惠券

--- src/main/java/com/o0u0o/missyou/repository/SkuRepository.java
+++ src/main/java/com/o0u0o/missyou/repository/SkuRepository.java
@@ -3,6 +3,7 @@ package com.o0u0o.missyou.repository;
 
 import com.o0u0o.missyou.model.Sku;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -25,14 +26,15 @@ public interface SkuRepository extends JpaRepository<Sku, Long> {
     List<Sku> findAllByIdIn(List<Long> ids);
 
     /**
-     * 减库存(乐观锁思想)
+     * 减库存(乐观锁思想) 乐观并发控制
      * @param sid skuID
      * @param quantity 减少的数量
      * @return
      */
+    @Modifying
     @Query("update Sku s set s.stock = s.stock - :quantity\n " +
             "where s.id = :sid\n " +
             "and s.stock >= :quantity")
-    int reduceStock(Long sid, Long quantity);
+    int reduceStock(Long sid, Integer quantity);
 
 }
--- src/main/java/com/o0u0o/missyou/repository/UserCouponRepository.java
+++ src/main/java/com/o0u0o/missyou/repository/UserCouponRepository.java
@@ -3,6 +3,8 @@ package com.o0u0o.missyou.repository;
 import com.o0u0o.missyou.model.User;
 import com.o0u0o.missyou.model.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -22,7 +24,26 @@ public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
      * 根据用户id和优惠券id查询用户的优惠券
      * @param uid 用户id
      * @param couponId 优惠券id
+     * @param status 优惠券状态 1-已经领取，但未使用
      * @return
      */
-    Optional<UserCoupon> findFirstByUserIdAndCouponId(Long uid, Long couponId);
+    Optional<UserCoupon> findFirstByUserIdAndCouponIdAndStatus(Long uid, Long couponId, int status);
+
+    /**
+     * 核销优惠券（要防止优惠券可以多次使用）
+     * status-1:优惠券已领取未使用
+     * orderId is null：优惠券未使用
+     * @param couponId 优惠券id
+     * @param oid 当前订单id
+     * @param uid 用户id
+     * @return
+     */
+    @Modifying
+    @Query("update UserCoupon uc set \n" +
+            "uc.status = 2, uc.orderId = :oid  \n" +
+            "where uc.couponId = : couponId\n " +
+            "and uc.userId = :uid\n " +
+            "and uc.status = 1 \n" +
+            "and uc.orderId is null")
+    int writeOff(Long couponId, Long oid, Long uid);
 }
