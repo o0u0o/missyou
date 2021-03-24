16d3fa79d2a4062eac18a658e4b7e203ee8aaeaa
岳川贵 <jobs@techgeng.com>
2021-03-24 09:18:49 +0800

    :sparkles: 获取客户端的远程IP

---
+++ src/main/java/com/o0u0o/missyou/common/utils/HttpRequestProxy.java
@@ -0,0 +1,67 @@
+package com.o0u0o.missyou.common.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 获取客户端的远程IP
+ * @author o0u0o
+ * @date 2021/3/22 11:49 上午
+ */
+@Component
+public class HttpRequestProxy {
+    private static HttpServletRequest request;
+
+    @Autowired
+    public void setRequest(HttpServletRequest request) {
+        HttpRequestProxy.request = request;
+    }
+
+    public static String getRequestUrl() {
+        return HttpRequestProxy.request.getRequestURI();
+    }
+
+    /**
+     * 获取用户的远程IP地址
+     * @return
+     */
+    public static String getRemoteRealIp() {
+        HttpServletRequest request = HttpRequestProxy.request;
+        String ipAddress = null;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (ipAddress.equals("127.0.0.1")) {
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+
+        return ipAddress;
+    }
+}
