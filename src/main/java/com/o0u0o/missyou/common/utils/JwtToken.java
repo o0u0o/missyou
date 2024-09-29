4b2e5cd0349016b903813a913f5b7483454f2c76
yuechuangui <yuechuangui@gmail.com>
2024-09-29 14:25:49 +0800

    文档更新

--- src/main/java/com/o0u0o/missyou/common/utils/JwtToken.java
+++ src/main/java/com/o0u0o/missyou/common/utils/JwtToken.java
@@ -6,8 +6,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.Verification;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
